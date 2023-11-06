b5f7a22a3db35e0131b989a9ce370f09b46fcda2
pooranjoyb <pooranjoyb2016@gmail.com>
2023-11-06 22:46:25 +0530

    UI for the application complete

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.title}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          <Task text={'test1'} />
+          <Task text={'test2'} />
+        </View>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.input}>
+        <View style={styles.inputContainer}>
+          <View>
+            <TextInput style={styles.textInput} placeholder={'Write a Task'} />
+          </View>
+          <View style={styles.addBtn}>
+            <TouchableOpacity>
+              <Text style={styles.btnText}>+</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +41,59 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#E6EBF0'
+  },
+
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  input: {
+    position: 'absolute',
+    width: '88%',
     backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 12,
+    display: 'flex',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    bottom: 10,
+    marginBottom: 30,
+    marginHorizontal: 20,
+  },
+
+  inputContainer: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  addBtn: {
+    display: 'flex',
+    width: 30,
+    height: 30,
+    borderRadius: 5,
+    backgroundColor: '#7BE382',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  btnText: {
+
+    fontSize: 24,
+    color: 'green',
+    fontWeight: 'bold',
+  },
+
+  textInput: {
+    fontSize: 20
   },
 });
+
---
+++ components/Task.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+
+export default function Task(props) {
+    return (
+        <>
+            <View style={styles.item}>
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <Text style={styles.task}>
+                    {props.text}
+                </Text>
+            </View>
+        </>
+    )
+};
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 12,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    task: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        fontSize: 16,
+    },
+
+    square: {
+        width: 24,
+        height: 25,
+        borderRadius: 4,
+        marginRight: 15,
+        backgroundColor: '#55BCF9',
+        opacity: 0.8,
+    },
+})
